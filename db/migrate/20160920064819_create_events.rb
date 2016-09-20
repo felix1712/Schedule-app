eff4c794357b41175dfd2adebeef0c57b439b911
felix1712 <felix.andrean17@gmail.com>
2016-09-20 16:20:54 +0700

    tabulation and change comment on gemfile

--- db/migrate/20160920064819_create_events.rb
+++ db/migrate/20160920064819_create_events.rb
@@ -1,10 +1,10 @@
 class CreateEvents < ActiveRecord::Migration[5.0]
   def change
     create_table :events do |t|
-        t.text :summary
-        t.datetime :start_at
-        t.datetime :end_at
-        t.string :location
+      t.text :summary
+      t.datetime :start_at
+      t.datetime :end_at
+      t.string :location
 
       t.timestamps
     end
